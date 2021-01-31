aa8a7d6fdf5117dec0075ac02b44d6700b8d9c3b
Kevin Vinhas <kevin@vinhas.fr>
2021-01-31 13:06:37 +0100

    Adding batch_id parameter to the api call

--- app/services/matcher_api.rb
+++ app/services/matcher_api.rb
@@ -1,15 +1,11 @@
 class MatcherApi
-  def call(email:, payrolls_url:, teachers_url:)
+  def call(params:)
     uri = URI.parse(ENV.fetch("CUTIRO_API_URL"))
     http = Net::HTTP.new(uri.host)
 #    http.use_ssl = true
 
     request = Net::HTTP::Get.new(uri.path, {"Content-Type" => "application/json"})
-    request.body = {
-      email: email,
-      payrolls: payrolls_url,
-      teachers: teachers_url,
-    }.to_json
+    request.body = params.to_json
 
     http.request(request)
   end
