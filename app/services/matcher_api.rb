521e9f726a13e21e60171890c4c067bb72c0cd39
Kevin Vinhas <kevin@vinhas.fr>
2021-01-31 14:44:41 +0100

    Turn http request into a post

--- app/services/matcher_api.rb
+++ app/services/matcher_api.rb
@@ -2,10 +2,11 @@ class MatcherApi
   def call(params:)
     uri = URI.parse(ENV.fetch("CUTIRO_API_URL"))
     http = Net::HTTP.new(uri.host)
-#    http.use_ssl = true
 
-    request = Net::HTTP::Get.new(uri.path, {"Content-Type" => "application/json"})
+    request = Net::HTTP::Post.new(uri.path)
     request.body = params.to_json
+    request['Content-Type'] = 'application/json'
+
 
     http.request(request)
   end
