5ae842cacae195244998170b830a81c7543c6c49
Kevin Vinhas <kevin@vinhas.fr>
2021-01-31 18:09:20 +0100

    Add a get route to ease the pain from lambdas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: "welcome#index"
 
-  resource :cleaning, only: [:new, :create, :show, :update]
+  resource :cleaning, only: [:new, :create, :show]
+  get '/cleaning_refresh/:id/:result_url', to: 'cleaning#refresh'
+
 end
